984982a46097a0b023e1a210db505b6c9d32d2c1
fa103555 <antoine.fadda-rodriguez@etu.univ-cotedazur.fr>
2023-03-31 18:39:57 +0200

    Envoi de la notification de confirmation de commande

--- Android/app/src/main/java/pro/seinksansdoozebank/app512/views/MainActivity.java
+++ Android/app/src/main/java/pro/seinksansdoozebank/app512/views/MainActivity.java
@@ -2,8 +2,11 @@ package pro.seinksansdoozebank.app512.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageButton;
@@ -22,6 +25,7 @@ public class MainActivity extends AppCompatActivity implements CarAdapterListene
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createNotificationChannel();
         ImageButton button = findViewById(R.id.purchases_button);
         ListView listView = findViewById(R.id.car_list);
         listView.setAdapter(new CarAdapter(this));
@@ -42,6 +46,19 @@ public class MainActivity extends AppCompatActivity implements CarAdapterListene
     @Override
     public void onBackPressed() {
         moveTaskToBack(true);
+    }
+
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(getString(R.string.channel_id), name, importance);
+            channel.setDescription(description);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 
 }
--- Android/app/src/main/java/pro/seinksansdoozebank/app512/views/PaymentActivity.java
+++ Android/app/src/main/java/pro/seinksansdoozebank/app512/views/PaymentActivity.java
@@ -1,9 +1,19 @@
 package pro.seinksansdoozebank.app512.views;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.core.text.HtmlCompat;
 
+import android.Manifest;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.ImageButton;
 
@@ -18,11 +28,26 @@ public class PaymentActivity extends AppCompatActivity {
         ImageButton backButton = findViewById(R.id.back_button);
         int carId = getIntent().getIntExtra("carId", 0);
         backButton.setOnClickListener(v -> finish());
-
         Button buyButton = findViewById(R.id.buy_button);
         buyButton.setOnClickListener(v -> {
+            sendConfirmationNotification();
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
         });
+    }
+
+    private void sendConfirmationNotification() {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, getString(R.string.channel_id))
+                .setSmallIcon(R.drawable.car)
+                .setAutoCancel(true)
+                .setContentTitle(HtmlCompat.fromHtml("<font color=\"" + getColor(R.color.blue) + "\"><b>" + getString(R.string.notification_title) + "</b></font>", HtmlCompat.FROM_HTML_MODE_LEGACY))
+                .setContentText(getString(R.string.notification_text))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            Log.d("512Cars", "PERMISSION NOT GRANTED");
+            return;
+        }
+        notificationManager.notify(1, builder.build());
     }
 }
