79cb7db2bb8301e3e755b27108094291a8c39d7a
fa103555 <antoine.fadda-rodriguez@etu.univ-cotedazur.fr>
2023-04-04 19:57:07 +0200

    Corrections de warnings (27 left)

--- Android/app/src/main/java/pro/seinksansdoozebank/app512/views/PaymentActivity.java
+++ Android/app/src/main/java/pro/seinksansdoozebank/app512/views/PaymentActivity.java
@@ -178,11 +178,11 @@ public class PaymentActivity extends AppCompatActivity {
      * Fonction qui vérifie si les informations de paiement sont valides
      */
     private void checkPayement() {
-        boolean cardNumberValid = cardNumber.getText().toString().matches("^(?:4[0-9]{12}(?:[0-9]{3})?|[25][1-7][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\\d{3})\\d{11})$");
+        boolean cardNumberValid = cardNumber.getText().toString().matches("^(?:4\\d{12}(?:\\d{3})?|[25][1-7]\\d{14}|6(?:011|5\\d\\d)\\d{12}|3[47]\\d{13}|3(?:0[0-5]|[68]\\d)\\d{11}|(?:2131|1800|35\\d{3})\\d{11})$");
         boolean firstNameValid = firstName.getText().toString().matches("^[a-zA-Z]+$");
         boolean lastNameValid = lastName.getText().toString().matches("^[a-zA-Z]+$");
-        boolean cvvValid = cvv.getText().toString().matches("^[0-9]{3}$");
-        boolean dateValid = dateExpiration.getText().toString().matches("^(0[1-9]|1[0-2])/([0-9]{2})$");
+        boolean cvvValid = cvv.getText().toString().matches("^\\d{3}$");
+        boolean dateValid = dateExpiration.getText().toString().matches("^(0[1-9]|1[0-2])/(\\d{2})$");
         if (cardNumberValid
                 && firstNameValid
                 && lastNameValid
